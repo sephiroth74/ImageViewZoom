c3ef0c875523d26f3e40ccc4e6fe84db743a4b02
Alessandro Crugnola <alessandro@aviary.com>
2013-11-15 13:34:35 -0500

    gradle

--- imageviewzoom/build/source/buildConfig/release/it/sephiroth/android/library/imagezoom/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package it.sephiroth.android.library.imagezoom;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = false;
-
-}
